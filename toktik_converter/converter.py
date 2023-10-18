97b5c4bd889c1d9d748f85e20715057cd53cf620
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2023-10-19 03:49:08 +0700

    feat: added function to convert raw to mp4

--- toktik_converter/converter.py
+++ toktik_converter/converter.py
@@ -1,31 +1,13 @@
+from pathlib import Path
+import os
+
 import boto3
 from botocore.client import Config
-from botocore.exceptions import ClientError
-import os
 from dotenv import load_dotenv
-from pathlib import Path
+import ffmpeg
 
 
-def download_file_from_s3(object_name: str) -> Path:
-    """
-    Downloads a file from S3
-
-    Args:
-        object_name (str): The key of the object to download
-
-    Returns:
-        Path: The path to where the file was downloaded
-    """
-    load_dotenv()
-    s3 = boto3.client(
-        "s3",
-        region_name=os.environ.get("S3_REGION"),
-        endpoint_url=os.environ.get("S3_RAW_ENDPOINT"),
-        aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
-        aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY"),
-        config=Config(s3={"addressing_style": "virtual"}, signature_version="v4"),
-    )
-
+def download_file_from_s3(s3, object_name):
     file_name, file_extension = object_name.split(".")
     temp_folder = Path("/tmp") / file_name
     temp_folder.mkdir(parents=True, exist_ok=True)
@@ -35,5 +17,25 @@ def download_file_from_s3(object_name: str) -> Path:
     return download_target
 
 
+def convert_to_mp4(file_path: Path):
+    file_name, file_extension = os.path.splitext(file_path)
+    ffmpeg.input(file_path).output(f"{file_name}.mp4", vcodec="libx264").run(quiet=True)
+
+    if file_extension != "mp4":
+        os.remove(file_path)
+
+    return Path(f"{file_name}.mp4")
+
+
 if __name__ == "__main__":
-    download_file_from_s3("cupfka.mp4")
+    load_dotenv()
+    s3 = boto3.client(
+        "s3",
+        region_name=os.environ.get("S3_REGION"),
+        endpoint_url=os.environ.get("S3_RAW_ENDPOINT"),
+        aws_access_key_id=os.environ.get("S3_ACCESS_KEY"),
+        aws_secret_access_key=os.environ.get("S3_SECRET_ACCESS_KEY"),
+        config=Config(s3={"addressing_style": "virtual"}, signature_version="v4"),
+    )
+    downloaded_path = download_file_from_s3(s3, "IMG_6376.MOV")
+    converted_path = convert_to_mp4(downloaded_path)
